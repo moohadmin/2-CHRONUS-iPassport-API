d7d7b749d8a9fabe0d9d27406b8de36ccc5655f4
txai mattos <txai.mattos@solutis.com.br>
2021-02-10 15:31:51 -0300

    Initial commit

---
+++ iPassport/iPassport.Api/Configurations/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace iPassport.Api.Configurations
+{
+    /// Dependency Injection Class
+    public static class DependencyInjection
+    {
+        /// Dependency Injection Register
+        public static void RegisterDependencyInjection(IServiceCollection services) => ConfigureServiceRepository(services);
+
+        /// Dependency Injection Configure
+        public static void ConfigureServiceRepository(IServiceCollection services)
+        {
+
+        }
+    }
+}
