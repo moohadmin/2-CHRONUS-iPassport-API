f9656e8b4fc2d4a1c26630eff64597215fb109d6
txai mattos <txai.mattos@solutis.com.br>
2021-02-19 09:05:44 -0300

    MTC-50 Remove unsed fields

--- iPassport/iPassport.Api/Models/Requests/UserImageRequest.cs
+++ iPassport/iPassport.Api/Models/Requests/UserImageRequest.cs
@@ -1,9 +1,7 @@
 ﻿namespace iPassport.Api.Models.Requests
 {
     public class UserImageRequest
-    {
-        //public string FileName { get; set; }
-        //public System.Guid UserId { get; set; }
+    {        
         public Microsoft.AspNetCore.Http.IFormFile ImageFile { get; set; }
     }
 }
