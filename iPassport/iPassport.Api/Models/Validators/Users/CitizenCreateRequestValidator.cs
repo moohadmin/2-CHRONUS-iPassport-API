0e634c7c91ea54515249a1e4aa5aa0730d07ef31
José Alfredo de Souza Júnior <jose.alfredo@solutis.com.br>
2021-03-08 16:01:03 -0300

    MTC-149 fix validation

--- iPassport/iPassport.Api/Models/Validators/Users/CitizenCreateRequestValidator.cs
+++ iPassport/iPassport.Api/Models/Validators/Users/CitizenCreateRequestValidator.cs
@@ -24,16 +24,19 @@ namespace iPassport.Api.Models.Validators.Users
                 .SetValidator(new AddressValidator(localizer, false));
 
             RuleFor(x => x.Birthday)
+                .Cascade(CascadeMode.Stop)
                 .SetValidator(new RequiredFieldValidator<DateTime>("Birthday", localizer))
                 .LessThanOrEqualTo(DateTime.UtcNow).WithMessage(string.Format(localizer["InvalidField"], "Birthday"))
                 .GreaterThanOrEqualTo(DateTime.UtcNow.AddYears(-200)).WithMessage(string.Format(localizer["InvalidField"], "Birthday"));
             
-            RuleFor(x => x.Cpf).Cascade(CascadeMode.Stop)
+            RuleFor(x => x.Cpf)
+                .Cascade(CascadeMode.Stop)
                  .Length(11).WithMessage(string.Format(localizer["InvalidField"], "CPF"))
                  .Must(x => Regex.IsMatch(x, "^[0-9]+$")).WithMessage(string.Format(localizer["InvalidField"], "CPF"))
                  .Must(x => CpfUtils.Valid(x)).WithMessage(string.Format(localizer["InvalidField"], "CPF"));
 
             RuleFor(x => x.Cns)
+                .Cascade(CascadeMode.Stop)
                 .Length(15).WithMessage(string.Format(localizer["InvalidField"], "CNS"))
                 .Must(y => Regex.IsMatch(y, "^[0-9]+$"))
                 .WithMessage(string.Format(localizer["InvalidField"], "CNS"));
@@ -57,15 +60,15 @@ namespace iPassport.Api.Models.Validators.Users
                 .Length(13)
                     .WithMessage(string.Format(localizer["InvalidField"], "Phone"))
                 .Must(y => y.Substring(4, 1).Equals("9"))
-                    .WithMessage(string.Format(localizer["InvalidField"], "Phone"));
-            
-            RuleFor(x => x.Telephone).Must(y => Regex.IsMatch(y, "^[0-9]+$"))
+                    .WithMessage(string.Format(localizer["InvalidField"], "Phone"))
+                .Must(y => Regex.IsMatch(y, "^[0-9]+$"))
                 .WithMessage(string.Format(localizer["InvalidField"], "Phone"));
 
             RuleFor(x => x.NumberOfDoses)
                 .Must(x => x >= 0).WithMessage(string.Format(localizer["RequiredField"], "NumberOfDoses"));
 
             RuleForEach(x => x.Doses)
+                .Cascade(CascadeMode.Stop)
                 .NotNull().When(x => x.NumberOfDoses > 0).WithMessage(string.Format(localizer["RequiredField"], "Doses"))
                 .SetValidator(new UserVaccineCreateRequestValidator(localizer)).When(x => x.NumberOfDoses > 0);
         }
