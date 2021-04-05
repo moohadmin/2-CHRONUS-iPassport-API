034f4ab0522fc0d64f43dae94b2b6debb8eb4a35
José Alfredo de Souza Júnior <jose.alfredo@solutis.com.br>
2021-04-05 14:11:54 -0300

    MTC-382 fix page size constructor

--- iPassport/iPassport.Api/Models/Requests/Shared/PageFilterRequest.cs
+++ iPassport/iPassport.Api/Models/Requests/Shared/PageFilterRequest.cs
@@ -14,5 +14,24 @@
         /// Page Size
         /// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public PageFilterRequest()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 10;
+        }
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="pageNumber">Page Number</param>
+        /// <param name="pageSize">Page Size</param>
+        public PageFilterRequest(int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            this.PageSize = pageSize > 10 ? 10 : pageSize;
+        }
     }
 }
