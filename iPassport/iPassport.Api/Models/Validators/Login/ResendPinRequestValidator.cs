2f0cc196a3d0d936b44e7ab1d4b95f29a6abd3d1
Anderson Oliveira <anderson.oliveira@solutis.com.br>
2021-04-06 10:21:03 -0300

    MTC-424 - Fix validation messages to get field names from resources file

--- iPassport/iPassport.Api/Models/Validators/Login/ResendPinRequestValidator.cs
+++ iPassport/iPassport.Api/Models/Validators/Login/ResendPinRequestValidator.cs
@@ -13,13 +13,13 @@ namespace iPassport.Api.Models.Validators.Plans
             RuleFor(s => s.Phone)
                .Cascade(CascadeMode.Stop)
                .NotEmpty()
-                   .WithMessage(string.Format(localizer["InvalidField"], "Phone"))
+                   .WithMessage(string.Format(localizer["InvalidField"], localizer["Telephone"]))
                .Must(y => {
                    return !y.StartsWith("55") || (y.Substring(4, 1).Equals("9") && y.Length == 13);
                     })
-                    .WithMessage(string.Format(localizer["InvalidField"], "Phone"))
+                    .WithMessage(string.Format(localizer["InvalidField"], localizer["Telephone"]))
                 .Must(y => Regex.IsMatch(y, "^[0-9]+$"))
-                    .WithMessage(string.Format(localizer["InvalidField"], "Phone"));
+                    .WithMessage(string.Format(localizer["InvalidField"], localizer["Telephone"]));
 
             RuleFor(s => s.UserId)
                 .SetValidator(new GuidValidator("UserId", localizer));
