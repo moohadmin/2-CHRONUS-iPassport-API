9aca2e4c7afac21303d7c0eec525b1193b7b6143
José Alfredo de Souza Júnior <jose.alfredo@solutis.com.br>
2021-04-06 15:31:26 -0300

    MTC-372 fix priority group validation

--- iPassport/iPassport.Api/Controllers/PriorityGroupController.cs
+++ iPassport/iPassport.Api/Controllers/PriorityGroupController.cs
@@ -54,7 +54,6 @@ namespace iPassport.Api.Controllers
         [ProducesResponseType(typeof(BussinessExceptionResponse), 400)]
         [ProducesResponseType(typeof(ServerErrorResponse), 500)]
         [HttpGet]
-        [AuthorizeRole(RolesModel.Admin)]
         public async Task<ActionResult> GetByNameParts([FromQuery] GetByNamePartsPagedRequest request)
         {
             var res = await _service.FindByNameParts(_mapper.Map<GetByNamePartsPagedFilter>(request));
