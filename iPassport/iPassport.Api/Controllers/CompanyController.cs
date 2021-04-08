29ac5e5324f0263a98b534d5cd8b6c7781372e93
José Alfredo <jose.alfredo@solutis.com.br>
2021-04-08 14:54:54 -0300

    MTC-445 - Add mock return API

--- iPassport/iPassport.Api/Controllers/CompanyController.cs
+++ iPassport/iPassport.Api/Controllers/CompanyController.cs
@@ -1,16 +1,20 @@
 ﻿using AutoMapper;
 using iPassport.Api.Models;
 using iPassport.Api.Models.Requests;
+using iPassport.Api.Models.Requests.Company;
 using iPassport.Api.Models.Requests.Shared;
 using iPassport.Api.Models.Responses;
 using iPassport.Api.Security;
 using iPassport.Application.Interfaces;
 using iPassport.Application.Models;
+using iPassport.Application.Models.Pagination;
+using iPassport.Application.Models.ViewModels;
 using iPassport.Domain.Dtos;
 using iPassport.Domain.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace iPassport.Api.Controllers
@@ -103,6 +107,40 @@ namespace iPassport.Api.Controllers
         {
             var res = await _service.GetById(id);
             return Ok(res);
+        }
+
+        /// <summary>
+        /// This API is responsible for Get paged list of Headquarters Companies by name.
+        /// </summary>
+        /// <param name="request">Get Headquarter Company Paged Request</param>
+        /// <response code="200">Server returns Ok</response>
+        /// <response code="400">Bussiness Exception</response>
+        /// <response code="401">Token invalid or expired</response>
+        /// <response code="500">Due to server problems, it is not possible to get your data now</response> 
+        /// <returns>Paged Headquarters Companies list.</returns>
+        [ProducesResponseType(typeof(ResponseApi), 200)]
+        [ProducesResponseType(typeof(BussinessExceptionResponse), 400)]
+        [ProducesResponseType(typeof(ServerErrorResponse), 500)]
+        [HttpGet("Headquarter")]
+        [AuthorizeRole(RolesModel.Admin, RolesModel.Business)]
+
+        public async Task<ActionResult> GetHeadquartersCompanies([FromQuery] GetHeadquarterCompanyPagedRequest request)
+        {
+
+            var res = new List<HeadquarterCompanyViewModel>() { 
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "94802367000172", Name = "Empresa 1"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "66364363000114", Name = "Empresa 2"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "32412148000120", Name = "Empresa 3"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "88400978000191", Name = "Empresa 4"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "35044227000113", Name = "Empresa 5"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "11397374000109", Name = "Empresa 6"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "37655229000174", Name = "Empresa 7"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "55358824000120", Name = "Empresa 8"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "07178740000161", Name = "Empresa 9"},
+                new HeadquarterCompanyViewModel() { Id = Guid.NewGuid(), Cnpj = "29617892000156", Name = "Empresa 10"},
+            };
+            
+            return Ok(new PagedResponseApi(true, "Headquarters Companies", 1, 10, 1, 10, res));
         }
     }
 }
---
+++ iPassport/iPassport.Application/Models/ViewModels/HeadquarterCompanyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace iPassport.Application.Models.ViewModels
+{
+    public class HeadquarterCompanyViewModel
+    {
+        public Guid Id { get; set; }
+        public string Cnpj { get; set; }
+        public string Name { get; set; }
+    }
+}
