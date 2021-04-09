1139cbed623318557e461387f68a607580725aec
txai mattos <txai.mattos@solutis.com.br>
2021-04-09 12:58:42 -0300

    MTC-248 Mock Api

--- iPassport/iPassport.Api/Models/Requests/Company/CompanyCreateRequest.cs
+++ iPassport/iPassport.Api/Models/Requests/Company/CompanyCreateRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace iPassport.Api.Models.Requests
+﻿using System;
+
+namespace iPassport.Api.Models.Requests
 {
     /// <summary>
     /// Company Create Request model
@@ -10,13 +12,50 @@
         /// </summary>
         public string Name { get; set; }
         /// <summary>
+        /// Company Trade Name
+        /// </summary>
+        public string TradeName { get; set; }
+        /// <summary>
         /// Company Cnpj
         /// </summary>
         public string Cnpj { get; set; }
         /// <summary>
         /// Company Address
         /// </summary>
+        //TODO Txai Ajustar Validação da cidade
         public AddressCreateRequest Address { get; set; }
-        
+        /// <summary>
+        /// Company Segment
+        /// </summary>
+        public Guid? SegmentId { get; set; }
+        /// <summary>
+        /// Company Is Headquarters
+        /// </summary>
+        public bool? IsHeadquarters { get; set; }
+        /// <summary>
+        /// Parent Company
+        /// </summary>
+        public Guid? ParentId { get; set; }
+        /// <summary>
+        /// Responsible Person Name
+        /// </summary>
+        public string ResponsiblePersonName { get; set; }
+        /// <summary>
+        /// Responsible Person Occupation
+        /// </summary>
+        public string ResponsiblePersonOccupation { get; set; }
+        /// <summary>
+        /// Responsible Person Email
+        /// </summary>
+        public string ResponsiblePersonEmail { get; set; }
+        //TODO Txai Ver se téra telefone fixo tbm
+        /// <summary>
+        /// Responsible Person Phone
+        /// </summary>
+        public string ResponsiblePersonPhone { get; set; }
+        /// <summary>
+        /// Company Is Active
+        /// </summary>
+        public bool? IsActive { get; set; }
     }
 }
