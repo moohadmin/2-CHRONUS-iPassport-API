7e7b8b0f82283eadf1792a9a9e635a606418ead1
txai mattos <txai.mattos@solutis.com.br>
2021-04-29 11:53:30 -0300

    MTC-477 Fiz return message and add validation to inactivation to government companie too

--- iPassport/iPassport.Application/Services/CompanyService.cs
+++ iPassport/iPassport.Application/Services/CompanyService.cs
@@ -226,7 +226,7 @@ namespace iPassport.Application.Services
             if (isEdit)
             {
                 ValidatePrivateCompanyIsHeadquartersChange(editedCompany, dto);
-                ValidatePrivateCompanyDeactivate(editedCompany, dto);
+                ValidateCompanyDeactivate(editedCompany, dto);
             }
 
             await ValidateAddress(cityId);
@@ -413,9 +413,9 @@ namespace iPassport.Application.Services
             if (editedCompany.Segment.IsPrivateType() && editedCompany.IsHeadquarters != dto.IsHeadquarters)
                 throw new BusinessException(_localizer["NotAllowEditCompanyHeadquarters"]);
         }
-        private void ValidatePrivateCompanyDeactivate(Company editedCompany, CompanyAbstractDto dto)
+        private void ValidateCompanyDeactivate(Company editedCompany, CompanyAbstractDto dto)
         {
-            if (!dto.IsActive.Value && editedCompany.IsActive() && editedCompany.IsPrivateHeadquarters() && editedCompany.HasActiveSubsidiaries())
+            if (!dto.IsActive.Value && editedCompany.IsActive() && editedCompany.HasActiveSubsidiaries())
                 throw new BusinessException(_localizer["NotAllowDeactivateCompanyWithActiveSubsidiaries"]);
         }
         #endregion
