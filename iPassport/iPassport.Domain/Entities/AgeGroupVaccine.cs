bde36abf7935e74e5ed322cc2097019e2ff8412d
Jose Alfredo Junior <jose.alfredo@solutis.com.br>
2021-05-24 14:28:37 -0300

    MTC-583 remove not null from period type id

--- iPassport/iPassport.Domain/Entities/AgeGroupVaccine.cs
+++ iPassport/iPassport.Domain/Entities/AgeGroupVaccine.cs
@@ -6,7 +6,7 @@ namespace iPassport.Domain.Entities
     public class AgeGroupVaccine : Entity
     {
         public Guid VaccineId { get; private set; }
-        public Guid PeriodTypeId { get; private set; }
+        public Guid? PeriodTypeId { get; private set; }
         public int RequiredDoses { get; private set; }
         public int MaxTimeNextDose { get; private set; }
         public int MinTimeNextDose { get; private set; }
