da119d9f6154690ecd16778f6c7c62eb74711790
Alex <alexgamas@gmail.com>
2021-09-21 20:40:21 -0300

    remove unused vcalidator

--- iPassport/iPassport.Api/Configurations/Filters/ValidateModelStateFilterAttribute.cs
+++ iPassport/iPassport.Api/Configurations/Filters/ValidateModelStateFilterAttribute.cs
@@ -17,11 +17,13 @@ namespace iPassport.Api.Configurations.Filters
         /// <param name="context">Action Executing Context</param>
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if (!context.ModelState.IsValid)
+            var modelState = context.ModelState;
+
+            if (!modelState.IsValid)
             {
                 List<string> errors;
 
-                errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
+                errors = modelState.Values.Where(v => v.Errors.Count > 0)
                         .SelectMany(v => v.Errors)
                         .Select(v => v.ErrorMessage)
                         .ToList();
